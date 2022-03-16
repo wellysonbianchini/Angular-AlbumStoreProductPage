269434b50b97db05d73cf30f4334f5f0b88db21e
Wellyson <wellysonbianchini@gmail.com>
2022-03-16 14:51:00 -0300

    Parte 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    HttpClientModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
